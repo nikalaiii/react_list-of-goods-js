7f87a13a5a1458a5041d0c31ac8023f6445e7677
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2024-12-17 16:39:56 +0100

    prablemka

--- src/App.jsx
+++ src/App.jsx
@@ -14,70 +14,118 @@ export const goodsFromServer = [
   'Jam',
   'Garlic',
 ];
+
 const SORT_MODE_ALPHABET = 'alphabet';
 const SORT_MODE_LENGTH = 'length';
 const SORT_MODE_REVERSE = 'reverse';
 const SORT_MODE_RESET = 'reset';
 
-function setSort(goods, sortMode) {
-  let sortedGoods = [...goods];
+export const App = () => {
+  const [sortMode, setSortMode] = useState('');
+  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
 
-  if (goods && sortMode) {
-    switch (sortMode) {
+  function setSort(sortType) {
+    let sortedGoods;
+
+    switch (sortType) {
       case SORT_MODE_ALPHABET:
-        sortedGoods.sort((good1, good2) => good1.localeCompare(good2));
+        sortedGoods = [...goodsFromServer] // Always start with start array
+          .sort((good1, good2) => good1.localeCompare(good2));
         break;
 
       case SORT_MODE_LENGTH:
-        sortedGoods.sort((good1, good2) => good1.length - good2.length);
+        sortedGoods = [...goodsFromServer].sort(
+          (good1, good2) => good1.length - good2.length,
+        );
         break;
 
       case SORT_MODE_REVERSE:
-        sortedGoods.reverse();
+        sortedGoods = [...visibleGoods] // reverse start array
+          .reverse();
         break;
 
       case SORT_MODE_RESET:
-        sortedGoods = goodsFromServer;
+        sortedGoods = [...goodsFromServer]; // start array
         break;
+
       default:
-        return 'error!';
+        return;
     }
+
+    setVisibleGoods(sortedGoods);
   }
-
-  return sortedGoods;
-}
-
-export const App = () => {
-  const [sortMode, setSortMode] = useState('');
-  const visibleGoods = setSort(goodsFromServer, sortMode);
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
-          onClick={() => {setSortMode(SORT_MODE_ALPHABET)}}
+          className={
+            sortMode === SORT_MODE_ALPHABET
+              ? 'button is-info'
+              : 'button is-info is-light'
+          }
+          onClick={() => {
+            setSortMode(SORT_MODE_ALPHABET);
+            setSort(SORT_MODE_ALPHABET);
+          }}
         >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={
+            sortMode === SORT_MODE_LENGTH
+              ? 'button is-success'
+              : 'button is-success is-light'
+          }
+          onClick={() => {
+            setSortMode(SORT_MODE_LENGTH);
+            setSort(SORT_MODE_LENGTH);
+          }}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={
+            sortMode === SORT_MODE_REVERSE
+              ? 'button is-warning'
+              : 'button is-warning is-light'
+          }
+          onClick={() => {
+            setSortMode(SORT_MODE_REVERSE);
+            setSort(SORT_MODE_REVERSE);
+          }}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {sortMode && (
+          <button
+            type="button"
+            className={
+              sortMode === SORT_MODE_RESET
+                ? 'button is-danger'
+                : 'button is-danger is-light'
+            }
+            onClick={() => {
+              setSortMode(SORT_MODE_RESET);
+              setSort(SORT_MODE_RESET);
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         {visibleGoods.map(good => (
-          <li data-cy="Good">{good}</li>
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
         ))}
       </ul>
     </div>
