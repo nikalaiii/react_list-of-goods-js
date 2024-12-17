83e520659f87d8d12542e54f6772e95e1f0e084a
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2024-12-17 18:37:43 +0100

    fixeeeeed!!!! yea!!!

--- src/App.jsx
+++ src/App.jsx
@@ -14,41 +14,34 @@ export const goodsFromServer = [
   'Jam',
   'Garlic',
 ];
-
 const SORT_MODE_ALPHABET = 'alphabet';
 const SORT_MODE_LENGTH = 'length';
-const SORT_MODE_REVERSE = 'reverse';
-const SORT_MODE_RESET = 'reset';
+
+function setSort(goods, sortType, isReverse) {
+  const sortedGoods = [...goods];
+
+  switch (sortType) {
+    case SORT_MODE_ALPHABET:
+      sortedGoods.sort((a, b) => a.localeCompare(b));
+      break;
+    case SORT_MODE_LENGTH:
+      sortedGoods.sort((a, b) => a.length - b.length);
+      break;
+    default:
+      break;
+  }
+
+  if (isReverse) {
+    sortedGoods.reverse();
+  }
+
+  return sortedGoods;
+}
 
 export const App = () => {
   const [sortMode, setSortMode] = useState('');
-  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
-
-  function setSort(goods, sortType, isReverse) {
-    let sortedGoods = [...goods];
-
-    switch (sortType) {
-      case SORT_MODE_ALPHABET:
-        sortedGoods.sort((a, b) => a.localeCompare(b));
-        break;
-      case SORT_MODE_LENGTH:
-        sortedGoods.sort((a, b) => a.length - b.length);
-        break;
-      case SORT_MODE_RESET:
-        sortedGoods = [...goodsFromServer];
-        break;
-      case SORT_MODE_REVERSE:
-        break;
-      default:
-        break;
-    }
-
-    if (isReverse) {
-      sortedGoods.reverse();
-    }
-
-    return sortedGoods;
-  }
+  const [isReverse, setIsReverse] = useState(false);
+  const visibleGoods = setSort(goodsFromServer, sortMode, isReverse);
 
   return (
     <div className="section content">
@@ -61,13 +54,11 @@ export const App = () => {
               : 'button is-info is-light'
           }
           onClick={() => {
-            setVisibleGoods(setSort(visibleGoods, SORT_MODE_ALPHABET, false));
             setSortMode(SORT_MODE_ALPHABET);
           }}
         >
           Sort alphabetically
         </button>
-
         <button
           type="button"
           className={
@@ -76,46 +67,35 @@ export const App = () => {
               : 'button is-success is-light'
           }
           onClick={() => {
-            setVisibleGoods(setSort(visibleGoods, SORT_MODE_LENGTH, false));
             setSortMode(SORT_MODE_LENGTH);
           }}
         >
           Sort by length
         </button>
-
         <button
           type="button"
           className={
-            sortMode === SORT_MODE_REVERSE
-              ? 'button is-warning'
-              : 'button is-warning is-light'
+            isReverse ? 'button is-warning' : 'button is-warning is-light'
           }
           onClick={() => {
-            setVisibleGoods(setSort(visibleGoods, SORT_MODE_REVERSE, true));
-            setSortMode(SORT_MODE_REVERSE)
+            setIsReverse(reverse => !reverse);
           }}
         >
           Reverse
         </button>
-
-        {sortMode && (
+        {(sortMode || isReverse) && (
           <button
             type="button"
-            className={
-              sortMode === SORT_MODE_RESET
-                ? 'button is-danger'
-                : 'button is-danger is-light'
-            }
+            className="button is-danger is-light"
             onClick={() => {
-              setVisibleGoods(setSort(visibleGoods, SORT_MODE_RESET, false));
-              setSortMode(SORT_MODE_RESET);
+              setSortMode('');
+              setIsReverse(false);
             }}
           >
             Reset
           </button>
         )}
       </div>
-
       <ul>
         {visibleGoods.map(good => (
           <li key={good} data-cy="Good">
